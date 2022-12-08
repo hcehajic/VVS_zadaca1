d1174fa49486c2b7b1b4ac019465274f40ce9848
Adnan <ahajro2@etf.unsa.ba>
2022-12-08 16:07:49 +0100

    Napravljena klasa za test F3

---
+++ TestProject/Funkcionalnost3Test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadaca1;
+
+namespace TestProject
+{
+    [TestClass]
+    public class Funkcionalnost3Test
+    {
+        public void pripremiPodatke()
+        {
+
+        }
+        [TestMethod]
+        public void provjeraIspisaStranaka1()
+        {
+            
+        }
+    }
+
+
+}
