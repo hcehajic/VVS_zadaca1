dd19ee45bdeed6b22f5c19fd0e9e27249b4f2b26
Aldin Kulaglić <akulaglic3@etf.unsa,ba>
2023-01-07 12:53:03 +0100

    com

--- TestProject/ObuhvatTest.cs
+++ TestProject/ObuhvatTest.cs
@@ -4,16 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Zadaca1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestProject
 {
     [TestClass]
-    internal class ObuhvatTest
+    public class ObuhvatTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestPotpuniObuhvatOdluka()
         {
-            AssertFalse(Kandidat.ValidirajBrojLicneKarte("123A32"));
+            Assert.IsFalse(Kandidat.ValidirajBrojLicneKarte("123A32"));
+            Assert.IsFalse(Kandidat.ValidirajBrojLicneKarte("C000E0009"));
+            Assert.IsFalse(Kandidat.ValidirajBrojLicneKarte("0000A0009"));
+            Assert.IsTrue(Kandidat.ValidirajBrojLicneKarte("0000E0009"));
+        }
+
+        [TestMethod]
+        public void TestPotpuniObuhvatUslova()
+        {
+            Assert.IsFalse(Kandidat.ValidirajBrojLicneKarte("123A32"));
+            Assert.IsFalse(Kandidat.ValidirajBrojLicneKarte("C000A0009"));
+            Assert.IsFalse(Kandidat.ValidirajBrojLicneKarte("C000E0009"));
+            Assert.IsFalse(Kandidat.ValidirajBrojLicneKarte("0000A0009"));
+            Assert.IsTrue(Kandidat.ValidirajBrojLicneKarte("0000E0009"));
         }
     }
 }
