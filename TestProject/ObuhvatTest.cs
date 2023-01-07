107fb7dd47fae64a0558dbb40e267768e00431aa
Aldin Kulaglić <akulaglic3@etf.unsa,ba>
2023-01-07 12:31:06 +0100

    dfsd

---
+++ TestProject/ObuhvatTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadaca1;
+
+namespace TestProject
+{
+    [TestClass]
+    internal class ObuhvatTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AssertFalse(Kandidat.ValidirajBrojLicneKarte("123A32"));
+        }
+    }
+}
